ab82c42b8ed129923d99f35cc07910bc2e201b83
Ross Kulyk <uaross@hotmail.com>
2022-01-24 19:51:09 -0500

    Updated showPage component with setError and setIsLoading states, added clean-up func to useEffect

--- src/components/pages/ShowPage.js
+++ src/components/pages/ShowPage.js
@@ -6,16 +6,45 @@ export default function ShowPage() {
     // custom hook
     const { id } = useParams();
     const [show, setShow] = useState(null);
-    console.log("ShowPage useParams:id > ", id);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMsg, setError] = useState(null);
 
     useEffect(() => {
-        console.log("ShowPage use effect run!");
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((result) => {
-            setShow(result);
-        });
+        // Var to indicate if component is mounted or not, and do clean-up
+        let isMounted = true;
+
+        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+            .then((result) => {
+                if (isMounted) {
+                    setShow(result);
+                    setIsLoading(false);
+                }
+                // TESTING:
+                // setTimeout(() => {
+                //     if (isMounted) {
+                //         setShow(result);
+                //         setIsLoading(false);
+                //     }
+                // }, 2000);
+            })
+            .catch((err) => {
+                if (isMounted) {
+                    setError(err.message);
+                    setIsLoading(false);
+                }
+            });
+        // do the cleanup
+        return () => {
+            // console.log("ShowPage useEffect cleanup ");
+            isMounted = false;
+        };
     }, [id]);
 
-    console.log("ShowPage showState", show);
+    console.log("ShowPage show > ", show);
 
-    return <div>Single Show Page</div>;
+    if (isLoading) return <div>Data is loading</div>;
+
+    if (errorMsg) return <div>Error is occured: {errorMsg}</div>;
+
+    return <div>Single Show Page:</div>;
 }
