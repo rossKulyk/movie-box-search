8d2fdcd9befe18908d3f8ab12dec1ffbeb46d365
Ross Kulyk <uaross@hotmail.com>
2022-01-27 09:08:56 -0500

    implemented changes to do the deployment to github pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
+// HashRouter configure the app for client side routing ONLY
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById("root")
 );
 
